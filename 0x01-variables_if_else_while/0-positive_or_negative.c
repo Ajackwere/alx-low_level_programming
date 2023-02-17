b3f996802febacd295c4726bae9eaf06053135f9
Ajackwere <austinewere59@gmail.com>
2023-02-17 10:14:09 -0800

    removed strand

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 /**
@@ -8,19 +9,19 @@ int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - Entry
+ * Return: 0;
+ */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					do {
+						printf("%d\n", n);
+						n++;
+					} while (
+					return (0);
+}
+
