49318d0c6fe7bac32f444e77ff79b97d318bd511
Ajackwere <austinewere59@gmail.com>
2023-02-21 11:32:17 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*
- * main - Entry
- * Description: 'A program that prints _putchar followed by new line.'
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
