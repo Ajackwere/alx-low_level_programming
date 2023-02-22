dd6e1d1d60bb4bcf461da555c090457daa30468b
Ajackwere <austinewere59@gmail.com>
2023-02-21 19:02:48 -0800

    updated times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * time_table - prin the 9 times tables
+ * times_table - prin the 9 times tables
  */
 void times_table(void)
 {
@@ -18,7 +18,7 @@ void times_table(void)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + '0');
-		
+
 		_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
